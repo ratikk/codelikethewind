21f30be26d56ef9f7d2ed7c4ff14e05232c1fddb
ratikk <ratik.nanda@gmail.com>
2022-07-28 19:55:38 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 echo `pwd`
 echo 'hello'
 touch  /var/lib/jenkins/workspace/openshift/show.txt
-echo `jfrog config show`
+echo `jfrog -v`
 echo 'hello1'
 jfrog rt u "/var/lib/jenkins/workspace/openshift/target/simple-servlet-0.0.1-SNAPSHOT.war" "test-maven-oc/simple-servlet-0.0.1-SNAPSHOT-$BUILD_NUMBER.war" --recursive=false
 
